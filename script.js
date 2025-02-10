2247b902c6d166807b5c69bc459d3e47ccec2d81
manish tayal <manishk82182@gmail.com>
2025-02-10 11:03:03 +0530

    half projecte complet

--- script.js
+++ script.js
@@ -6,3 +6,184 @@ var swiper = new Swiper(".hero-section", {
         el: ".swiper-pagination",
       },
 });
+
+// top game
+var swiper = new Swiper(".mySwiper", {
+            slidesPerView: 5,
+            spaceBetween: 15,
+            loop:true,
+            navigation: {
+                nextEl: ".swiper-button-next-tg",
+                prevEl: ".swiper-button-prev-tg",
+            },
+            breakpoints: {
+                320: {
+                    slidesPerView: 2,
+                },
+                400: {
+                    slidesPerView: 3,
+                },
+                640: {
+                    slidesPerView: 3,
+                },
+                768: {
+                    slidesPerView: 3,
+                },
+                1024: {
+                    slidesPerView: 4,
+                },
+                1200: {
+                    slidesPerView: 5,
+                },
+            },
+});
+
+//royaloriginal
+var swiper = new Swiper(".mySwiper2", {
+            slidesPerView: 5,
+            spaceBetween: 15,
+            loop:true,
+            navigation: {
+                nextEl: ".swiper-button-next-ro",
+                prevEl: ".swiper-button-prev-ro",
+            },
+            breakpoints: {
+                320: {
+                    slidesPerView: 2,
+                },
+                400: {
+                    slidesPerView: 3,
+                },
+                640: {
+                    slidesPerView: 3,
+                },
+                768: {
+                    slidesPerView: 3,
+                },
+                1024: {
+                    slidesPerView: 4,
+                },
+                1200: {
+                    slidesPerView: 5,
+                },
+            },
+});
+
+//tournament
+var swiper = new Swiper(".mySwiper3", {
+            slidesPerView: 3,
+            spaceBetween: 24,
+            loop:true,
+            navigation: {
+                nextEl: ".swiper-button-next-trnmt",
+                prevEl: ".swiper-button-prev-trnmt",
+            },
+            breakpoints: {
+                320: {
+                    slidesPerView: 1,
+                },
+                500: {
+                    slidesPerView: 1.5,
+                },
+                768: {
+                    slidesPerView: 1.8,
+                },
+                1024: {
+                    slidesPerView: 2,
+                },
+                1200: {
+                    slidesPerView: 2.5,
+                },
+                1500: {
+                    slidesPerView: 2.7,
+                },
+                1600: {
+                    slidesPerView: 3,
+                },
+            },
+});
+
+// about-us
+var swiper = new Swiper(".mySwiper4", {
+    spaceBetween:16,
+    slidesPerView: 2,
+    loop:true,
+    pagination: {
+        el: ".swiper-pagination",
+      },
+    
+      breakpoints: {
+                320: {
+                    slidesPerView: 1,
+                },
+                500: {
+                    slidesPerView: 2,
+                },
+            }
+});
+
+// jeckpo
+// swiper-5
+
+// top winner
+var swiper = new Swiper(".mySwiper6", {
+            slidesPerView: 4,
+            spaceBetween: 16,
+            loop:true,
+            navigation: {
+                nextEl: ".swiper-button-next-tw",
+                prevEl: ".swiper-button-prev-tw",
+            },
+            breakpoints: {
+                320: {
+                    slidesPerView: 1,
+                },
+                400: {
+                    slidesPerView: 2,
+                },
+                1000: {
+                    slidesPerView: 3,
+                },
+                1300: {
+                    slidesPerView: 3,
+                },
+                1500: {
+                    slidesPerView: 4,
+                },
+                1800: {
+                    slidesPerView: 5,
+                },
+            },
+});
+
+// recent- big wind
+var swiper = new Swiper(".mySwiper7", {
+            slidesPerView: 4,
+            spaceBetween: 16,
+            loop:true,
+            // autoplay:true,
+            navigation: {
+                nextEl: ".swiper-button-next-rbw",
+                prevEl: ".swiper-button-prev-rbw",
+            },
+            breakpoints: {
+                320: {
+                    slidesPerView: 1,
+                },
+                400: {
+                    slidesPerView: 2,
+                },
+                1000: {
+                    slidesPerView: 3,
+                },
+                1300: {
+                    slidesPerView: 3,
+                },
+                1500: {
+                    slidesPerView: 4,
+                },
+                1800: {
+                    slidesPerView: 5,
+                },
+            },
+});
